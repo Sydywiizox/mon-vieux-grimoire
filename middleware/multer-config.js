d5b61311f00a81a42a2edbed498cecf17d3b7a7c
Sydywiizox <andrea_duhamel@outlook.fr>
2025-01-17 11:59:02 +0100

    fusion des middleware pour les images

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,69 +1,78 @@
 const multer = require("multer");
-const sharp = require("sharp"); // Ajoutez cette ligne
+const sharp = require("sharp");
 const fs = require("fs");
 const path = require("path");
 
+// Vérifie et crée le dossier "images" si nécessaire
+const ensureImagesFolderExists = () => {
+  if (!fs.existsSync("images")) {
+    fs.mkdirSync("images");
+  }
+};
+
+// Configuration du stockage Multer
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
+    ensureImagesFolderExists(); // Assure que le dossier existe
     callback(null, "images");
   },
   filename: (req, file, callback) => {
-    const name = file.originalname.split(" ").join("_"); //remplace les espaces par des '_'
-    const extension = path.parse(file.originalname).ext; //récupère l'extension
-    callback(null, path.parse(name).name + Date.now() + extension); //path.parse(name).name enlève l'extension du fichier original
+    const name = path.parse(file.originalname.split(" ").join("_")).name; // Remplace les espaces par des '_'
+    const extension = path.parse(file.originalname).ext; // Récupère l'extension
+    callback(null, name + Date.now() + extension); // Génère un nom unique
   },
 });
 
-const upload = multer({ storage: storage }).single("image");
+const uploadAndOptimizeImage = (req, res, next) => {
+  const upload = multer({ storage: storage }).single("image");
 
-// Middleware d'optimisation des images
-const optimizeImage = async (req, res, next) => {
-  if (!fs.existsSync("images")) {
-    fs.mkdirSync("images");
-  }
-  if (!req.file) return next();
+  upload(req, res, async (err) => {
+    if (err) {
+      return next(err);
+    }
 
-  try {
-    const filePath = req.file.path;
-    const fileName = path.parse(req.file.filename).name;
-    const outputPath = `images/${fileName}.webp`;
+    if (!req.file) {
+      return next();
+    }
 
-    // Convertit et optimise l'image
-    await sharp(filePath)
-      .webp({ quality: 80 })
-      .resize(800, 1000, {
-        fit: "inside",
-        withoutEnlargement: true,
-      })
-      .toFile(outputPath);
+    try {
+      const filePath = req.file.path;
+      const fileName = path.parse(req.file.filename).name;
+      const outputPath = `images/${fileName}.webp`;
 
-    console.log("Chemin complet :", filePath);
-    console.log(fs.existsSync(filePath));
-    fs.unlink(filePath, (err) => {
-      if (err) {
-        console.error(
-          "Erreur lors de la suppression du fichier original:",
-          err
-        );
-      } else {
-        console.log("Fichier supprimé avec succès");
-      }
-    });
+      // Convertit et optimise l'image
+      await sharp(filePath)
+        .webp({ quality: 80 })
+        .resize(800, 1000, {
+          fit: "inside",
+          withoutEnlargement: true,
+        })
+        .toFile(outputPath);
 
-    // Met à jour les informations de fichier dans la requête
-    req.file.filename = `${fileName}.webp`;
-    req.file.path = outputPath;
-    next();
-  } catch (error) {
-    next(error);
-  }
+      // Supprime le fichier original
+      fs.unlink(filePath, (err) => {
+        if (err) {
+          console.error(
+            "Erreur lors de la suppression du fichier original:",
+            err
+          );
+        } else {
+          console.log("Fichier supprimé avec succès");
+        }
+      });
+
+      // Met à jour les informations de fichier dans la requête
+      req.file.filename = `${fileName}.webp`;
+      req.file.path = outputPath;
+      next();
+    } catch (error) {
+      console.error("Erreur : Echec de traitement de l'image", error);
+      return res.status(500).json({
+        message: "Erreur : Echec de traitement de l'image",
+        error: error.message,
+      });
+    }
+  });
 };
 
-const createFolderImages = (req, res, next) => {
-  if (!fs.existsSync("images")) {
-    fs.mkdirSync("images");
-  }
-  next();
-};
-
-module.exports = { upload, optimizeImage, createFolderImages };
+module.exports = { uploadAndOptimizeImage };
--- routes/book.js
+++ routes/book.js
@@ -1,32 +1,14 @@
 const express = require("express");
 const auth = require("../middleware/auth");
 const router = express.Router();
-const {
-  upload,
-  optimizeImage,
-  createFolderImages,
-} = require("../middleware/multer-config");
+const { uploadAndOptimizeImage } = require("../middleware/multer-config");
 const booksCtrl = require("../controllers/book");
 
 router.get("/", booksCtrl.getAllBooks);
 router.get("/bestrating", booksCtrl.getBestRatedBooks);
 router.get("/:id", booksCtrl.getOneBook);
-router.post(
-  "/",
-  auth,
-  createFolderImages,
-  upload,
-  optimizeImage,
-  booksCtrl.createBook
-);
-router.put(
-  "/:id",
-  auth,
-  createFolderImages,
-  upload,
-  optimizeImage,
-  booksCtrl.modifyBook
-);
+router.post("/", auth, uploadAndOptimizeImage, booksCtrl.createBook);
+router.put("/:id", auth, uploadAndOptimizeImage, booksCtrl.modifyBook);
 router.delete("/:id", auth, booksCtrl.deleteBook);
 router.post("/:id/rating", auth, booksCtrl.addRating);
 
